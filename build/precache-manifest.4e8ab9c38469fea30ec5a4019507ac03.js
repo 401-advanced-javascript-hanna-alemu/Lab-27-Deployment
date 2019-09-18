88566870f637c4f5da7c17c89bff9957b82c30a3
hannaalemu <hanna9alemu@outlook.com>
2019-09-17 20:42:54 -0700

    fixed lint errors

---
+++ build/precache-manifest.4e8ab9c38469fea30ec5a4019507ac03.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6187d455272451398ce81a3b8fe65ff5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "adcf3f83933570960641",
+    "url": "/static/css/main.9cf95136.chunk.css"
+  },
+  {
+    "revision": "38c8de48536b1463faf1",
+    "url": "/static/js/2.5c350061.chunk.js"
+  },
+  {
+    "revision": "adcf3f83933570960641",
+    "url": "/static/js/main.9ab2c976.chunk.js"
+  },
+  {
+    "revision": "55abb783777d0218f8e1",
+    "url": "/static/js/runtime~main.f2105dc0.js"
+  }
+]);
--- src/__tests__/counter.test.js
+++ src/__tests__/counter.test.js
@@ -1,44 +1,46 @@
 import React from 'react';
+
 import renderer from 'react-test-renderer';
-import Counter from '../components/counter/counter';
+
 import Enzyme, { shallow, mount } from 'enzyme';
+
 import Adapter from 'enzyme-adapter-react-16';
+
+import Counter from '../components/counter/counter';
+
 Enzyme.configure({ adapter: new Adapter() });
  
 
 describe('< Counter />', () => {
-    it( 'Is rendered when app starts', () => {
-        let app = shallow(<Counter />);
+  it('Is rendered when app starts', () => {
+    const app = shallow(<Counter />);
 
-        expect(app.find('.down').exists()).toEqual(true);
-        expect(app.find('.up').exists()).toEqual(true);
+    expect(app.find('.down').exists()).toEqual(true);
+    expect(app.find('.up').exists()).toEqual(true);
 
-        expect(app.find('.count').text()).toEqual("0");
-    });
+    expect(app.find('.count').text()).toEqual('0');
+  });
 
-    it('count should be toggled when down clicker is pressed', () => {
-        let app = mount(< Counter />);
-        let downA = app.find('.down');
+  it('count should be toggled when down clicker is pressed', () => {
+    const app = mount(< Counter />);
+    const downA = app.find('.down');
 
-        downA.simulate('click');
-        expect(app.state().polarity).toEqual("negative");
-        expect(app.find('.count').text()).toEqual("-1");
-        
-    });
+    downA.simulate('click');
+    expect(app.state().polarity).toEqual('negative');
+    expect(app.find('.count').text()).toEqual('-1');
+  });
     
-    it('count should be toggled when up clicker is pressed', () => {
-        let app = mount(< Counter />);
-        let downA = app.find('.up');
+  it('count should be toggled when up clicker is pressed', () => {
+    const app = mount(< Counter />);
+    const downA = app.find('.up');
 
-        downA.simulate('click');
-        expect(app.state().polarity).toEqual("positive");
-        expect(app.find('.count').text()).toEqual("1");
-
-    });
+    downA.simulate('click');
+    expect(app.state().polarity).toEqual('positive');
+    expect(app.find('.count').text()).toEqual('1');
+  });
     
-    it('matches snapshot', () => {
-        const snapshot = renderer.create(< Counter />).toJSON();
-        expect(snapshot).toMatchSnapshot();
-    })
-
+  it('matches snapshot', () => {
+    const snapshot = renderer.create(< Counter />).toJSON();
+    expect(snapshot).toMatchSnapshot();
+  });
 });
